1abda8ad6b5c91fed7e6c0cfafb41a4516909e14
stat-ki <per_ik@outlook.com>
2020-03-06 05:10:37 +0000

    #1 Create relationship model & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	devise_for :users
   resources :users,only: [:show,:edit,:update,:index]
+  resources :relationships, only: [:create, :edit]
   root 'home#top'
   get 'home/about'
 
